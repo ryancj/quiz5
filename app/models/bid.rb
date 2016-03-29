5946c65f4180e2bbc2ea384b2caa775a49473880
Ryan Kayu Ching <ryanching@hotmail.com>
2016-03-29 14:53:28 -0700

    Fix current price if NIL one last time

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -2,5 +2,6 @@ class Bid < ActiveRecord::Base
   belongs_to :auction
 
   validates :price, presence: true,
-    format: { with: /\A(\$)?(\d+)(\.|,)?\d{0,2}?\z/ }
+    format: { with: /\A(\$)?(\d+)(\.|,)?\d{0,2}?\z/ },
+    allow_blank: false, numericality: true
 end
